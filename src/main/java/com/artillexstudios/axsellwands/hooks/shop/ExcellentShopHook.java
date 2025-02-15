059fc6ee270ffdb7f3a94c4c30499aa8e4cbf1e9
BenceX100 <birincsikbence@gmail.com>
2025-02-15 09:00:06 +0100

    1.8.2

--- src/main/java/com/artillexstudios/axsellwands/hooks/shop/ExcellentShopHook.java
+++ src/main/java/com/artillexstudios/axsellwands/hooks/shop/ExcellentShopHook.java
@@ -3,8 +3,8 @@ package com.artillexstudios.axsellwands.hooks.shop;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import su.nightexpress.nexshop.ShopAPI;
-import su.nightexpress.nexshop.api.shop.product.VirtualProduct;
 import su.nightexpress.nexshop.api.shop.type.TradeType;
+import su.nightexpress.nexshop.shop.virtual.impl.VirtualProduct;
 
 public class ExcellentShopHook implements PricesHook {
 
