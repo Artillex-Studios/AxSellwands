11491bda22f0631a1ae22bdb6e21218cbfcbb99e
ValdemarF20 <valdemarfoster.dk@gmail.com>
2025-04-29 16:44:09 +0200

    Fix for EcoShop hook

--- src/main/java/com/artillexstudios/axsellwands/hooks/shop/EcoShopHook.java
+++ src/main/java/com/artillexstudios/axsellwands/hooks/shop/EcoShopHook.java
@@ -1,23 +1,39 @@
 package com.artillexstudios.axsellwands.hooks.shop;
 
-import com.willfp.ecoshop.integrations.EcoShopAdapter;
 import com.willfp.eco.core.price.Price;
+import com.willfp.ecoshop.event.EcoShopSellEvent;
+import com.willfp.ecoshop.integrations.EcoShopAdapter;
+import com.willfp.ecoshop.shop.ShopItem;
+import com.willfp.ecoshop.shop.ShopItemUtils;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
 public class EcoShopHook implements PricesHook {
 
     @Override
-    public void setup() {
-    }
+    public void setup() {}
 
     @Override
     public double getPrice(ItemStack it) {
-        return 0; // EcoShop doesn't have a way to get the price of an item without a player
+        return 0;
     }
 
     @Override
     public double getPrice(Player player, ItemStack it) {
-        return EcoShopAdapter.INSTANCE.getUnitValue(it, player).getValue(player);
+        ShopItem shopItem = ShopItemUtils.getShopItem(it);
+        if (shopItem == null) {
+            return 0;
+        }
+
+        Price unitPrice = EcoShopAdapter.INSTANCE.getUnitValue(it, player);
+
+        EcoShopSellEvent event = new EcoShopSellEvent(player, shopItem, unitPrice, it);
+        Bukkit.getPluginManager().callEvent(event);
+
+        double multiplier = event.getMultiplier();
+        double baseValue   = unitPrice.getValue(player);
+
+        return baseValue * multiplier * it.getAmount();
     }
 }
