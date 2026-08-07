c0dd5196d6a260ade2622ad1f68ca2b03b5fa250
BenceX100 <52270269+BenceX100@users.noreply.github.com>
2026-08-07 19:51:40 +0200

    1.16.3

--- src/main/java/com/artillexstudios/axsellwands/AxSellwands.java
+++ src/main/java/com/artillexstudios/axsellwands/AxSellwands.java
@@ -82,7 +82,7 @@ public final class AxSellwands extends AxPlugin {
     }
 
     public void updateFlags() {
-        FeatureFlags.USE_LEGACY_HEX_FORMATTER.set(true);
+        FeatureFlags.USE_LEGACY_HEX_FORMATTER.set(false);
         FeatureFlags.PACKET_ENTITY_TRACKER_ENABLED.set(true);
         FeatureFlags.HOLOGRAM_UPDATE_TICKS.set(20L);
         FeatureFlags.PACKET_ENTITY_TRACKER_THREADS.set(1);
